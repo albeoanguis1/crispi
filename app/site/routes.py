1db807d86737f40958127d9caf8c9f00504d903f
Antonio Popoca <104789557+albeoanguis1@users.noreply.github.com>
2022-12-02 20:27:25 -0500

    working data

--- app/site/routes.py
+++ app/site/routes.py
@@ -17,8 +17,6 @@ def profile():
 
 @site.route('/search', methods = ["GET", "POST"])
 def search():
-    #TRYING TO IMPLEMENT IN JS
-
     url = "https://api.spoonacular.com/food/menuItems/search?"
     #Grabbing the input from the HTML form
     item = request.form.get('inputsearch')
@@ -26,7 +24,7 @@ def search():
 
     parameters = {
         'query' :{item},
-        'number' : 3,
+        'number' : 10,
         'apiKey' : '07607f40a346438790c194f4913ce4a3'
     }
 
@@ -41,23 +39,21 @@ def search():
     try:
         response = session.get(url, params=parameters)
         data = json.loads(response.text)
-
-        global dict
+        # global dict
         dict={}
-        if 'dict' in globals():
-            dict[item]= {
-                'title': data["menuItems"][0]["title"],
-                'image': data["menuItems"][0]["image"],
-                'restaurant': data["menuItems"][0]["restaurantChain"],
-                'id': data["menuItems"][0]["id"]
-            }
-        print(dict)
-        # return dict[item]
+        # if 'dict' in globals():
+        dict= {
+            'title': data["menuItems"][0]["title"],
+            'image': data["menuItems"][0]["image"],
+            'restaurant': data["menuItems"][0]["restaurantChain"],
+            'servingsize': data['menuItems'][0]["readableServingSize"],
+            'id': data["menuItems"][0]["id"]
+        }
+        print(data)
         
     except (ConnectionError, Timeout, TooManyRedirects) as error:
         print(error)
 
-    return render_template('search.html')
+    return render_template('search.html', dict=dict, item=item, data=data)
 
 
-test = 'hello'
